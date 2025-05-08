3f0af5ab4b6658af9540dacf852c886fd348198c
Sam Kim <samuel.kim.2898@gmail.com>
2025-05-08 15:08:01 -0400

    minor style change, updated screenshot

--- src/components/layout/Skills.tsx
+++ src/components/layout/Skills.tsx
@@ -31,7 +31,7 @@ function Skills() {
   return (
     <section id="skills" className="bg-gray-700 min-h-screen pt-20 pb-35">
       <h1 className="text-5xl font-bold text-center text-white mb-15">Skills</h1>
-      <div className="max-w-screen-xl px-4 grid grid-cols-1 sm:grid-cols-2 gap-6">
+      <div className="max-w-screen-xl mx-auto px-4 grid grid-cols-1 sm:grid-cols-2 gap-6">
         {skillCardElements}
       </div>
     </section>
